546b460b471a80fdcf2503c8b3a9e3ca2815cbac
Mims H. Wright <mwright@hugeinc.com>
2014-02-11 15:44:37 -0800

    Linted like crazy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
 module.exports = function(grunt) {
+    "use strict";
+
     grunt.initConfig({
 
         jsSrcPath: 'src',
@@ -13,33 +15,65 @@ module.exports = function(grunt) {
                 options: {
                 },
                 src: ['<%=jsSrcPath%>']
+            }
+        },
+
+        jsbeautifier : {
+            options : {
+                indentSize: 4,                // Indentation size [4]
+                indentChar: " ",              // Indentation character [space]
+                indentWithTabs: "false",
+                preserveNewlines: "false",    // Preserve line-breaks (--no-preserve-newlines disables)
+                maxPreserveNewlines: "4",     // Number of line-breaks to be preserved in one chunk [10]
+                spaceInParen: "true",         // Add padding spaces within paren, ie. f( a, b )
+                jslintHappy: "true",          // Enable jslint-stricter mode
+                braceStyle: "collapse",       // [collapse|expand|end-expand] ["collapse"]
+                breakChainedMethods: "true",  // Break chained method calls across subsequent lines
+                keepArrayIndentation: "true", // Preserve array indentation
+                unescapeStrings: "false",     // Decode printable characters encoded in xNN notation
+                wrapLineLength: "false",      // Wrap lines at next opportunity after N characters [0]
+                e4x: "true",                  // Pass E4X xml literals through untouched
+                goodStuff: "true"             // Warm the cockles of Crockford's heart
             },
+
+            dev: {
+                src : ["<%=jsSrcPath%>/**/*.js"],
+                options: {
+                    mode: "VERIFY_AND_WRITE"
+                }
+            },
+            deploy: {
+                src : ["<%=jsSrcPath%>/**/*.js"],
+                options: {
+                    mode: "VERIFY_ONLY"
+                }
+            }
         },
 
         uglify: {
             deploy: {
                 files: [{
-                    expand: true,     // Enable dynamic expansion.
+                    expand: true,               // Enable dynamic expansion.
                     cwd: '<%=jsSrcPath%>',      // Src matches are relative to this path.
-                    src: ['**/*.js'], // Actual pattern(s) to match.
-                    dest: '<%=deployPath%>',   // Destination path prefix.
-                    ext: '.min.js',   // Dest filepaths will have this extension.
+                    src: ['**/*.js'],           // Actual pattern(s) to match.
+                    dest: '<%=deployPath%>',    // Destination path prefix.
+                    ext: '.min.js'              // Dest filepaths will have this extension.
                 }]
             }
         },
 
         qunit: {
-                files: ['<%=testPath%>/**/*.html']
+            files: ['<%=testPath%>/**/*.html']
         },
 
         open: {
             test : {
-              path: '<%=testPath%>/index.html',
-              app: 'Google Chrome'
+                path: '<%=testPath%>/index.html',
+                app: 'Google Chrome'
             },
             demo : {
-              path: '<%=demoPath%>/index.html',
-              app: 'Google Chrome'
+                path: '<%=demoPath%>/index.html',
+                app: 'Google Chrome'
             }
         },
 
@@ -51,17 +85,17 @@ module.exports = function(grunt) {
         },
 
         bump: {
-          options: {
-            files: ['package.json'],
-            updateConfigs: [],
-            commit: true,
-            commitMessage: 'Version v%VERSION%',
-            commitFiles: ['package.json'], // '-a' for all files
-            createTag: true,
-            tagName: 'v%VERSION%',
-            tagMessage: 'Version %VERSION%',
-            push: false
-          }
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Version v%VERSION%',
+                commitFiles: ['package.json'], // '-a' for all files
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: false
+            }
         }
     });
 
@@ -72,14 +106,15 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-bump');
     grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-jsbeautifier');
 
     grunt.registerTask('default', ['dev']);
     grunt.registerTask('dev', ['js:dev']);
     grunt.registerTask('deploy', ['clean', 'js:deploy']);
 
     grunt.registerTask('js', ['js:dev']);
-    grunt.registerTask('js:dev', ['lint' ]);
-    grunt.registerTask('js:deploy', ['lint', 'uglify']);
+    grunt.registerTask('js:dev', ['lint', "jsbeautifier:dev" ]);
+    grunt.registerTask('js:deploy', ['lint', "jsbeautifier:deploy", 'uglify']);
     grunt.registerTask('lint', ['jshint']);
     grunt.registerTask('test', ['js:dev', 'open:test']);
     grunt.registerTask('demo', ['deploy', 'open:demo']);
--- src/pointbreak.js
+++ src/pointbreak.js
@@ -1,179 +1,202 @@
-var PointBreak = PointBreak || {};
+// js-hint globals
+/*global define,window,document*/
+/*exported PointBreak*/
 
-if (typeof define === "function" && define.amd) {
-  define("PointBreak", [], function() {
+var PointBreak = (function() {
+    "use strict";
+
+    /**
+     * Sets up a system for dispatching events when breakpoints change.
+     * @class PointBreak
+     * @constructor
+     */
+    PointBreak = function(breakpoints, targetWindow) {
+        var that = this,
+            name;
+
+        if (targetWindow !== undefined) {
+            this.window = targetWindow;
+        } else {
+            this.window = window;
+        }
+
+
+        this.breakpoints = {};
+
+
+        // Register default breakpoints.
+        if (PointBreak.defaultBreakpoints !== null) {
+            for (name in PointBreak.defaultBreakpoints) {
+                if (PointBreak.defaultBreakpoints.hasOwnProperty(name)) {
+                    this.registerBreakpoint(name, PointBreak.defaultBreakpoints[name]);
+                }
+            }
+        }
+
+        if (breakpoints !== undefined && breakpoints !== null) {
+            this.registerBreakpoint(breakpoints);
+        }
+
+        this.getWindow().addEventListener("resize", function(e) {
+            that.onResize(e);
+        });
+        document.addEventListener("load", function(e) {
+            that.onResize(e);
+        });
+    };
+
+    /** Shortcut for listening to the BREAKPOINT_CHANGE_EVENT on the window */
+    PointBreak.prototype.addChangeListener = function(handler) {
+        this.getWindow().addEventListener(PointBreak.BREAKPOINT_CHANGE_EVENT, handler);
+    };
+
+    /** Shortcut for un-listening to the BREAKPOINT_CHANGE_EVENT on the window */
+    PointBreak.prototype.removeChangeListener = function(handler) {
+        this.getWindow().removeEventListener(PointBreak.BREAKPOINT_CHANGE_EVENT, handler);
+    };
+
+    /** Returns the current width of the screen */
+    PointBreak.prototype.getWidth = function() {
+        return this.getWindow().innerWidth;
+    };
+
+    PointBreak.prototype.getWindow = function() {
+        return this.window;
+    };
+
+    /**
+     * Called when the window resizes.
+     */
+    PointBreak.prototype.onResize = function() {
+        var newWidth = this.getWidth(),
+            currentBreakpoint = this.getBreakpointForSize(newWidth),
+            breakpointChangeEvent,
+            specificBreakpointEvent;
+
+        if (this.lastBreakpoint !== currentBreakpoint) {
+            // Dispatch a generic event for this breakpoint change.
+            breakpointChangeEvent = document.createEvent("Event");
+            breakpointChangeEvent.initEvent(PointBreak.BREAKPOINT_CHANGE_EVENT, true, true);
+            breakpointChangeEvent.oldBreakpoint = this.lastBreakpoint;
+            breakpointChangeEvent.newBreakpoint = currentBreakpoint;
+            breakpointChangeEvent.width = newWidth;
+            this.getWindow().dispatchEvent(breakpointChangeEvent);
+
+            // Dispatch a special event type for this breakpoint name
+            // e.g. "smallBreakpoint"
+            specificBreakpointEvent = document.createEvent("Event");
+            specificBreakpointEvent.initEvent(currentBreakpoint + "Breakpoint", true, true);
+            specificBreakpointEvent.oldBreakpoint = this.lastBreakpoint;
+            specificBreakpointEvent.newBreakpoint = currentBreakpoint;
+            specificBreakpointEvent.width = newWidth;
+            this.getWindow().dispatchEvent(specificBreakpointEvent);
+            this.lastBreakpoint = currentBreakpoint;
+        }
+    };
+
+    /**
+     * Returns the array of breakpoints.
+     */
+    PointBreak.prototype.getBreakpoints = function() {
+        return this.breakpoints;
+    };
+
+    /**
+     * Returns the name for the width.
+     */
+    PointBreak.prototype.getBreakpointForSize = function(width) {
+        var lowestBreakpointValue = Infinity,
+            lowestBreakpointName = PointBreak.MAX_BREAKPOINT,
+            breakpointName,
+            breakpoint;
+
+        for (breakpointName in this.breakpoints) {
+            if (this.breakpoints.hasOwnProperty(breakpointName)) {
+                breakpoint = this.breakpoints[breakpointName];
+                if (width <= breakpoint && breakpoint <= lowestBreakpointValue) {
+                    lowestBreakpointValue = breakpoint;
+                    lowestBreakpointName = breakpointName;
+                }
+            }
+        }
+        return lowestBreakpointName;
+    };
+
+    PointBreak.prototype.getSizeOfBreakpoint = function(name) {
+        if (this.isBreakpoint(name)) {
+            return this.breakpoints[name];
+        }
+        return 0;
+    };
+
+    /**
+     * Returns the name for the current breakpoint.
+     */
+    PointBreak.prototype.getCurrentBreakpoint = function() {
+        return this.getBreakpointForSize(this.getWidth());
+    };
+
+    /**
+     * Add a breakpoint with `name` at the specified `width`.
+     */
+    PointBreak.prototype.registerBreakpoint = function(object_or_name, width) {
+        var obj, name;
+        if (typeof object_or_name === "string" && !isNaN(width)) {
+            name = object_or_name;
+            obj = {};
+            obj[name] = width;
+        } else {
+            obj = object_or_name;
+        }
+
+        for (name in obj) {
+            if (obj.hasOwnProperty(name)) {
+                this.registerSingleBreakpoint(name, obj[name]);
+            }
+        }
+    };
+    PointBreak.prototype.registerSingleBreakpoint = function(name, width) {
+        this.breakpoints[name] = width;
+
+        // Add a new version of "on" for this breakpoint.
+        var capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+        this["add" + capitalizedName + "Listener"] = function(handler) {
+            this.getWindow().addEventListener(name + "Breakpoint", handler);
+        };
+        this["remove" + capitalizedName + "Listener"] = function(handler) {
+            this.getWindow().removeEventListener(name + "Breakpoint", handler);
+        };
+    };
+
+    /**
+     * Add a breakpoint with `name` at the specified `width`.
+     */
+    PointBreak.prototype.unregisterBreakpoint = function(name) {
+        this.breakpoints[name] = null;
+        var capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+        this["add" + capitalizedName + "Listener"] =
+            this["remove" + capitalizedName + "Listener"] = null;
+    };
+
+    /**
+     * Returns true if the current breakpoint matches the `name` parameter.
+     */
+    PointBreak.prototype.isBreakpoint = function(name) {
+        return this.getBreakpointForSize(this.getWidth()) === name;
+    };
+
+    // Set any breakpoints to automatically add to every instance.
+    PointBreak.defaultBreakpoints = {};
+    PointBreak.prototype.BREAKPOINT_CHANGE_EVENT = "breakpointChange";
+    PointBreak.prototype.MAX_BREAKPOINT = "max";
+
     return PointBreak;
-  });
+}());
+
+// AMD / Require.js definition
+if (typeof define === "function" && define.amd) {
+    define("PointBreak", [], function() {
+        "use strict";
+        return PointBreak;
+    });
 }
-
-/**
- * Sets up a system for dispatching events when breakpoints change.
- * @class
- */
-PointBreak = function(breakpoints, targetWindow) {
-
-	var lastBreakpoint;
-	var that = this;
-
-	if ( targetWindow !== undefined ) {
-		this.window = targetWindow;
-	} else {
-		this.window = window;
-	}
-
-
-	this.breakpoints = {};
-
-	// Register default breakpoints.
-	if (PointBreak.defaultBreakpoints !== null) {
-		for (var name in PointBreak.defaultBreakpoints) {
-			this.registerBreakpoint(name, PointBreak.defaultBreakpoints[name]);
-		}
-	}
-
-	if (breakpoints !== undefined && breakpoints !== null) {
-		this.registerBreakpoint(breakpoints);
-	}
-
-	lastBreakpoint = this.getBreakpointForSize(this.getWidth());
-
-	this.getWindow().addEventListener("resize", function (e) { that.onResize(e); } );
-	document.addEventListener("load", function (e) { that.onResize(e); } );
-};
-
-/** Shortcut for listening to the BREAKPOINT_CHANGE_EVENT on the window */
-PointBreak.prototype.addChangeListener = function (handler) {
-	this.getWindow().addEventListener(PointBreak.BREAKPOINT_CHANGE_EVENT, handler);
-};
-
-/** Shortcut for un-listening to the BREAKPOINT_CHANGE_EVENT on the window */
-PointBreak.prototype.removeChangeListener = function (handler) {
-	this.getWindow().removeEventListener(PointBreak.BREAKPOINT_CHANGE_EVENT, handler);
-};
-
-/** Returns the current width of the screen */
-PointBreak.prototype.getWidth = function () {
-	return this.getWindow().innerWidth;
-};
-
-PointBreak.prototype.getWindow = function () {
-	return this.window;
-};
-
-/**
- * Called when the window resizes.
- */
-PointBreak.prototype.onResize = function (event) {
-	var newWidth = this.getWidth();
-	var currentBreakpoint = this.getBreakpointForSize(newWidth);
-	var breakpointChangeEvent, specificBreakpointEvent;
-	if (this.lastBreakpoint != currentBreakpoint) {
-		// Dispatch a generic event for this breakpoint change.
-		breakpointChangeEvent = document.createEvent("Event");
-		breakpointChangeEvent.initEvent(PointBreak.BREAKPOINT_CHANGE_EVENT, true, true);
-		breakpointChangeEvent.oldBreakpoint = this.lastBreakpoint;
-		breakpointChangeEvent.newBreakpoint = currentBreakpoint;
-		breakpointChangeEvent.width = newWidth;
-		this.getWindow().dispatchEvent(breakpointChangeEvent);
-
-		// Disptach a special event type for this breakpoint name
-		// e.g. "smallBreakpoint"
-		specificBreakpointEvent = document.createEvent("Event");
-		specificBreakpointEvent.initEvent(currentBreakpoint + "Breakpoint", true, true);
-		specificBreakpointEvent.oldBreakpoint = this.lastBreakpoint;
-		specificBreakpointEvent.newBreakpoint = currentBreakpoint;
-		specificBreakpointEvent.width = newWidth;
-		this.getWindow().dispatchEvent(specificBreakpointEvent);
-		this.lastBreakpoint = currentBreakpoint;
-	}
-};
-
-/**
- * Returns the array of breakpoints.
- */
-PointBreak.prototype.getBreakpoints = function() {
-	return this.breakpoints;
-};
-
-/**
- * Returns the name for the width.
- */
-PointBreak.prototype.getBreakpointForSize = function (width) {
-	var lowestBreakpointValue = Infinity;
-	var lowestBreakpointName = PointBreak.MAX_BREAKPOINT;
-	for (var breakpointName in this.breakpoints) {
-		var breakpoint = this.breakpoints[breakpointName];
-		if (width <= breakpoint && breakpoint <= lowestBreakpointValue ) {
-			lowestBreakpointValue = breakpoint;
-			lowestBreakpointName = breakpointName;
-		} else {
-		}
-	}
-	return lowestBreakpointName;
-};
-
-PointBreak.prototype.getSizeOfBreakpoint = function (name) {
-	if (this.isBreakpoint(name)) {
-		return this.breakpoints[name];
-	}
-	return 0;
-};
-
-/**
- * Returns the name for the current breakpoint.
- */
-PointBreak.prototype.getCurrentBreakpoint = function () {
-	return this.getBreakpointForSize(this.getWidth());
-};
-
-/**
- * Add a breakpoint with `name` at the specified `width`.
- */
-PointBreak.prototype.registerBreakpoint = function (object_or_name, width) {
-	var obj, name;
-	if (typeof(object_or_name) == "string" && !isNaN(width)) {
-		name = object_or_name;
-		obj = {};
-		obj[name] = width;
-	} else {
-		obj = object_or_name;
-	}
-
-	for (name in obj) {
-		this._registerSingleBreakpoint(name, obj[name]);
-	}
-};
-PointBreak.prototype._registerSingleBreakpoint = function(name, width) {
-	this.breakpoints[name] = width;
-
-	// Add a new version of "on" for this breakpoint.
-	var capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
-	this["add" + capitalizedName + "Listener"] = function (handler) {
-		this.getWindow().addEventListener(name + "Breakpoint", handler);
-	};
-	this["remove" + capitalizedName + "Listener"] = function (handler) {
-		this.getWindow().removeEventListener(name + "Breakpoint", handler);
-	};
-};
-
-/**
- * Add a breakpoint with `name` at the specified `width`.
- */
-PointBreak.prototype.unregisterBreakpoint = function (name) {
-	this.breakpoints[name] = null;
-	var capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
-	this["add" + capitalizedName + "Listener"] =
-	this["remove" + capitalizedName + "Listener"] = null;
-};
-
-/**
- * Returns true if the current breakpoint matches the `name` parameter.
- */
-PointBreak.prototype.isBreakpoint = function (name) {
-	return this.getBreakpointForSize(this.getWidth()) == name;
-};
-
-// Set any breakpoints to automatically add to every instance.
-PointBreak.defaultBreakpoints = {};
-PointBreak.BREAKPOINT_CHANGE_EVENT = "breakpointChange";
-PointBreak.MAX_BREAKPOINT = "max";
