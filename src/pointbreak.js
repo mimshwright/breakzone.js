e05364961546f274bb22c03703c504a05e728d1f
Mims H. Wright <mwright@hugeinc.com>
2014-02-11 17:42:10 -0800

    Updated tests and jsdocs

--- src/pointbreak.js
+++ src/pointbreak.js
@@ -9,6 +9,8 @@ var PointBreak = (function() {
      * Sets up a system for dispatching events when breakpoints change.
      * @class PointBreak
      * @constructor
+     * @param {BreakpointObject} [breakpoints] A set of breakpoints to register when PointBreak initializes.
+     * @param {Window} [targetWindow=window] If you want to target a different window than the current one, you may set it here.
      */
     PointBreak = function(breakpoints, targetWindow) {
         var that = this,
@@ -17,12 +19,11 @@ var PointBreak = (function() {
         if (targetWindow !== undefined) {
             this.window = targetWindow;
         } else {
+            // use the current global window by default.
             this.window = window;
         }
 
-
         this.breakpoints = {};
-
 
         // Register default breakpoints.
         if (PointBreak.defaultBreakpoints !== null) {
@@ -45,21 +46,38 @@ var PointBreak = (function() {
         });
     };
 
-    /** Shortcut for listening to the BREAKPOINT_CHANGE_EVENT on the window */
+    /**
+     * Shortcut for listening to the BREAKPOINT_CHANGE_EVENT on the window
+     *
+     * @param {BreakpointChangeHandler} handler
+     */
     PointBreak.prototype.addChangeListener = function(handler) {
         this.getWindow().addEventListener(PointBreak.BREAKPOINT_CHANGE_EVENT, handler);
     };
 
-    /** Shortcut for un-listening to the BREAKPOINT_CHANGE_EVENT on the window */
+    /**
+     * Shortcut for un-listening to the BREAKPOINT_CHANGE_EVENT on the window
+     *
+     * @param {BreakpointChangeHandler} handler
+     */
     PointBreak.prototype.removeChangeListener = function(handler) {
         this.getWindow().removeEventListener(PointBreak.BREAKPOINT_CHANGE_EVENT, handler);
     };
 
-    /** Returns the current width of the screen */
+    /**
+     * Returns the current width of the screen
+     *
+     * @return {number} width of screen.
+     */
     PointBreak.prototype.getWidth = function() {
         return this.getWindow().innerWidth;
     };
 
+    /**
+     * Returns PointBreak's target window.
+     *
+     * @returns {Window} window
+     */
     PointBreak.prototype.getWindow = function() {
         return this.window;
     };
@@ -95,7 +113,9 @@ var PointBreak = (function() {
     };
 
     /**
-     * Returns the array of breakpoints.
+     * Returns the array of registered breakpoints.
+     *
+     * @return {BreakpointObject} breakpoints.
      */
     PointBreak.prototype.getBreakpoints = function() {
         return this.breakpoints;
@@ -103,6 +123,9 @@ var PointBreak = (function() {
 
     /**
      * Returns the name for the width.
+     *
+     * @param {number} width Any width number to check the name of.
+     * @return {string} Breakpoint title for width.
      */
     PointBreak.prototype.getBreakpointForSize = function(width) {
         var lowestBreakpointValue = Infinity,
@@ -122,15 +145,23 @@ var PointBreak = (function() {
         return lowestBreakpointName;
     };
 
+    /**
+     * Returns the maximum width of the breakpoint with the specified name.
+     *
+     * @param {string} name Name of the breakpoint.
+     * @returns {number} The maximum width of the breakpoint.
+     */
     PointBreak.prototype.getSizeOfBreakpoint = function(name) {
-        if (this.isBreakpoint(name)) {
+        if (this.hasBreakpoint(name)) {
             return this.breakpoints[name];
         }
         return 0;
     };
 
     /**
-     * Returns the name for the current breakpoint.
+     * Returns the name for the window's current breakpoint.
+     *
+     * @return {string} Name of the current breakpoint.
      */
     PointBreak.prototype.getCurrentBreakpoint = function() {
         return this.getBreakpointForSize(this.getWidth());
@@ -138,9 +169,18 @@ var PointBreak = (function() {
 
     /**
      * Add a breakpoint with `name` at the specified `width`.
+     *
+     * @param {BreakpointObject|string} object_or_name If this value is a string, use it as the name
+     *                                  along with the width to register the breakpoint.
+     *                                  If this value is an object, inspect it as if it
+     *                                  was a hash of breakpoint names and widths.
+     * @param {number} [width] The maximum width of the breakpoint (if object_or_name was a string)
      */
     PointBreak.prototype.registerBreakpoint = function(object_or_name, width) {
         var obj, name;
+
+        // check to see if the object_or_name was a string.
+        // if so, convert it to an object format.
         if (typeof object_or_name === "string" && !isNaN(width)) {
             name = object_or_name;
             obj = {};
@@ -149,15 +189,46 @@ var PointBreak = (function() {
             obj = object_or_name;
         }
 
+        // loop through the object and register each breakpoint found.
         for (name in obj) {
             if (obj.hasOwnProperty(name)) {
                 this.registerSingleBreakpoint(name, obj[name]);
             }
         }
     };
+
+    /**
+     * @private
+     * Explicitly add a single breakpoint name / value pair.
+     *
+     * @param {string} name Name of the breakpoint.
+     * @param {number} width Max width of the breakpoint.
+     */
     PointBreak.prototype.registerSingleBreakpoint = function(name, width) {
         this.breakpoints[name] = width;
 
+        this.addListenerFunctionsForBreakpoint(name);
+    };
+
+    /**
+     * Remove the breakpoint called `name`.
+     *
+     * @param {string} name The name of the breakpoint to remove.
+     */
+    PointBreak.prototype.unregisterBreakpoint = function(name) {
+        this.breakpoints[name] = null;
+        this.removeListenerFunctionsForBreakpoint(name);
+    };
+
+    /**
+     * @private
+     * Creates two functions on the instance of PointBreak for adding and removing listeners.
+     * They are addNameListener() and removeNameListener() where "Name" would be replaced with
+     * the name of the breakpoint. E.g. addSmallListener()
+     *
+     * @param name The name of the breakpoint
+     */
+    PointBreak.prototype.addListenerFunctionsForBreakpoint = function(name) {
         // Add a new version of "on" for this breakpoint.
         var capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
         this["add" + capitalizedName + "Listener"] = function(handler) {
@@ -169,24 +240,53 @@ var PointBreak = (function() {
     };
 
     /**
-     * Add a breakpoint with `name` at the specified `width`.
+     * @private
+     * Removes the listener functions added by addListenerFunctionsForBreakpoint().
+     *
+     * @param {string} name The name of the breakpoint to remove the listeners for.
      */
-    PointBreak.prototype.unregisterBreakpoint = function(name) {
-        this.breakpoints[name] = null;
+    PointBreak.prototype.removeListenerFunctionsForBreakpoint = function(name) {
         var capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
         this["add" + capitalizedName + "Listener"] =
             this["remove" + capitalizedName + "Listener"] = null;
     };
 
     /**
-     * Returns true if the current breakpoint matches the `name` parameter.
+     * Returns true if the name of current breakpoint is registered.
+     *
+     * @param {string} name A breakpoint to check.
      */
-    PointBreak.prototype.isBreakpoint = function(name) {
-        return this.getBreakpointForSize(this.getWidth()) === name;
+    PointBreak.prototype.hasBreakpoint = function(name) {
+        return !isNaN(this.getBreakpoints()[name]);
     };
 
-    // Set any breakpoints to automatically add to every instance.
+    /**
+     * Checks all parameters against the name of the current breakpoint.
+     * Returns true if any of them match.
+     *
+     * @param {...string} name One or more breakpoint names.
+     *
+     * @example
+     * // returns true if the window is in the small or medium breakpoint zone.
+     * myPointBreak.isCurrentBreakpoint("small", "medium");
+     */
+    PointBreak.prototype.isCurrentBreakpoint = function(name) {
+        var i = 0,
+            l = arguments.length,
+            result = false,
+            nameToCheck;
+
+        for (; i < l; i++) {
+            nameToCheck = (arguments[i]);
+            result = this.getCurrentBreakpoint() === nameToCheck;
+        }
+        return result;
+    };
+
+    // You can add breakpoints to all instances of PointBreak by adding
+    // values to PointBreak.defaultBreakpoints.
     PointBreak.defaultBreakpoints = {};
+
     PointBreak.prototype.BREAKPOINT_CHANGE_EVENT = "breakpointChange";
     PointBreak.prototype.MAX_BREAKPOINT = "max";
 
@@ -200,3 +300,25 @@ if (typeof define === "function" && define.amd) {
         return PointBreak;
     });
 }
+
+
+/**
+ * An object that maps names to breakpoint sizes.
+ *
+ * @typedef {Object.<string, number>} BreakpointObject
+ */
+
+/**
+ * An event that is dispatched when a breakpoint changes.
+ *
+ * @typedef {Event} BreakpointChangeEvent
+ * @prop {string} newBreakpoint
+ * @prop {string} oldBreakpoint
+ * @prop {int} width
+ */
+
+/**
+ * An event handler for changes in breakpoint.
+ * @typedef {Function} BreakpointChangeHandler
+ * @param {BreakpointChangeEvent} event
+ */
--- test/tests.js
+++ test/tests.js
@@ -1,89 +1,105 @@
-try {
-  // try to force an error.
-  if (PointBreak.prototype);
-} catch (e) {
-  alert("PointBreak.js not found. Try running `grunt test`");
-}
+// js-hint globals
+/*global define,window,document,PointBreak,assert,QUnit*/
+(function () {
+    "use strict";
 
-module("Dependencies");
+    // test instance of pointbreak
+    var pb;
 
-var pb, w;
+    try {
+        // try to force a reference error.
+        if (PointBreak) {
+            console.log("PointBreak loaded.");
+        }
+    } catch (e) {
+        window.alert("PointBreak.js not found. Try running `grunt QUnit.test`");
+    }
 
-test("require PointBreak.js", function() {
-  ok (PointBreak, "PointBreak.js is loaded.");
-});
+    QUnit.module("Dependencies");
 
-module ("Registering breakpoints on a still page", {
-	setup: function () {
-		pb = new PointBreak();
-	},
-	teardown: function () {
-		pb = null;
-	}
-});
+    QUnit.test("require PointBreak.js", function (assert) {
+        assert.ok (PointBreak, "PointBreak.js is loaded.");
+    });
 
-test ("methods for seeting and getting breakpoints", function () {
-	equal (pb.getWidth(), window.innerWidth, "getWidth gets the current width of the window.");
-	pb.registerBreakpoint("test", pb.getWidth());
-	ok (pb.isBreakpoint("test"), "After registering the breakpoint, 'test' is the current breakpoint");
-	equal ( pb.getBreakpoints().test, pb.getWidth(), "The value set is the max for the breakpoint.");
-	equal ( pb.getCurrentBreakpoint(), "test", "current breakpoint maps to the name of the current breakpoint.");
-	equal( pb.getBreakpointForSize(pb.getWidth()), "test", "getBreakpointForSize() returns the name of the breakpoint at the given size.");
-	equal( pb.getSizeOfBreakpoint("test"), pb.getWidth(), "getSizeOfBreakpoint() returns the size of the breakpoint or 0.");
-	equal( pb.getSizeOfBreakpoint("bogus"), 0, "getSizeOfBreakpoint() returns the size of the breakpoint or 0.");
-	pb.unregisterBreakpoint("test");
-	equal (pb.getCurrentBreakpoint(), PointBreak.MAX_BREAKPOINT, "unregisterBreakpoint removes breakpoints. MAX_BREAKPOINT is the only default breakpoint.");
-});
+    QUnit.module ("Registering breakpoints on a still page", {
+        setup: function () {
+            pb = new PointBreak();
+        },
+        teardown: function () {
+            pb = null;
+        }
+    });
+
+    QUnit.test ("methods for setting and getting breakpoints", function (assert) {
+        assert.equal (pb.getWidth(), window.innerWidth, "getWidth gets the current width of the window.");
+        pb.registerBreakpoint("test", pb.getWidth());
+
+        assert.ok (pb.hasBreakpoint("test"), "After registering, 'test' is a registered breakpoint.");
+        assert.ok (!pb.hasBreakpoint("bogus"), "'bogus' wasn't ever registered.");
+
+        assert.ok (pb.isCurrentBreakpoint("test"), "'test' is the current breakpoint (since it's based on getWidth())");
+        assert.ok (pb.isCurrentBreakpoint("a", "b", "c", "test"), "isCurrentBreakpoint() takes multiple params and is true if any match");
+        assert.ok (!pb.isCurrentBreakpoint("a", "b", "c"), "isCurrentBreakpoint() fails if none match");
+
+        assert.equal ( pb.getBreakpoints().test, pb.getWidth(), "The value set is the max for the breakpoint.");
+        assert.equal ( pb.getCurrentBreakpoint(), "test", "current breakpoint maps to the name of the current breakpoint.");
+        assert.equal ( pb.getBreakpointForSize(pb.getWidth()), "test", "getBreakpointForSize() returns the name of the breakpoint at the given size.");
+        assert.equal ( pb.getSizeOfBreakpoint("test"), pb.getWidth(), "getSizeOfBreakpoint() returns the max size of the breakpoint or 0.");
+        assert.equal ( pb.getSizeOfBreakpoint("bogus"), 0, "getSizeOfBreakpoint() returns the size of the breakpoint or 0.");
+        pb.unregisterBreakpoint("test");
+        assert.equal (pb.getCurrentBreakpoint(), PointBreak.MAX_BREAKPOINT, "unregisterBreakpoint removes breakpoints. MAX_BREAKPOINT is the only default breakpoint.");
+    });
 
 
-module ("Responding to window resizing");
+    QUnit.module ("Responding to window resizing");
 
-asyncTest ("Resizing", function () {
-	var w = window.open();
-	w.resizeTo(500,500);
+    QUnit.asyncTest ("Resizing", function (assert) {
+        var w = window.open();
+        w.resizeTo(500,500);
 
-	var pb = new PointBreak(null, w);
-	pb.registerBreakpoint("small", 300);
-	pb.registerBreakpoint({"med": 600, "large": 900});
+        var pb = new PointBreak(null, w);
+        pb.registerBreakpoint("small", 300);
+        pb.registerBreakpoint({"med": 600, "large": 900});
 
-	expect(9);
+        QUnit.expect(9);
 
-	equal(pb.getWindow(), w, "getWindow() returns the window object");
+        assert.equal(pb.getWindow(), w, "getWindow() returns the window object");
 
-	setTimeout(onWindowLoad, 1000);
-	setTimeout(function () {
-		w.close();
-		start();
-	}, 2000);
+        setTimeout(onWindowLoad, 1000);
+        setTimeout(function () {
+            w.close();
+            QUnit.start();
+        }, 2000);
 
-	function onWindowLoad () {
-		console.log(pb.getCurrentBreakpoint());
-		console.log(pb.getWidth());
-		equal (pb.getCurrentBreakpoint(), "med", "A custom window can be the target of pointbreak.js " + pb.getWidth());
+        function onWindowLoad () {
+            console.log(pb.getCurrentBreakpoint());
+            console.log(pb.getWidth());
+            assert.equal (pb.getCurrentBreakpoint(), "med", "A custom window can be the target of pointbreak.js " + pb.getWidth());
 
-		pb.addChangeListener(onWindowChange);
-		pb.addLargeListener(onLargeBreakpoint);
+            pb.addChangeListener(onWindowChange);
+            pb.addLargeListener(onLargeBreakpoint);
 
-		w.resizeTo(800, 500);
-	}
+            w.resizeTo(800, 500);
+        }
 
-	function onWindowChange (event) {
-		ok (1, "Change listener is fired on width change.");
-		equal (event.oldBreakpoint, "med", "Change listener is provided the old breakpoint");
-		equal (event.newBreakpoint, "large", "and the new breakpoint");
-		pb.removeChangeListener(onWindowChange);
-	}
+        function onWindowChange (event) {
+            assert.ok (1, "Change listener is fired on width change.");
+            assert.equal (event.oldBreakpoint, "med", "Change listener is provided the old breakpoint");
+            assert.equal (event.newBreakpoint, "large", "and the new breakpoint");
+            pb.removeChangeListener(onWindowChange);
+        }
 
-	function onLargeBreakpoint (event) {
-		ok (1, "onLargeBreakpoint: Large listener is fired on width change.");
-		equal (event.oldBreakpoint, "med", "Change listener is provided the old breakpoint");
-		equal (event.newBreakpoint, "large", "and the new breakpoint");
-		equal (pb.getCurrentBreakpoint(), "large", "There's a reference to the window object stored in pointbreak.js");
-	}
-});
+        function onLargeBreakpoint (event) {
+            assert.ok (1, "onLargeBreakpoint: Large listener is fired on width change.");
+            assert.equal (event.oldBreakpoint, "med", "Change listener is provided the old breakpoint");
+            assert.equal (event.newBreakpoint, "large", "and the new breakpoint");
+            assert.equal (pb.getCurrentBreakpoint(), "large", "There's a reference to the window object stored in pointbreak.js");
+        }
+    });
 
-test ("Default breakpoints", function () {
-	PointBreak.defaultBreakpoints = {"xxx":123};
-	var pb = new PointBreak();
-	equal(pb.getBreakpointForSize(100), "xxx", "Defining default breakpoints makes the breakpoints automatically get added.");
-})
+    QUnit.test ("Default breakpoints", function (assert) {
+        PointBreak.defaultBreakpoints = {"xxx":123};
+        var pb = new PointBreak();
+        assert.equal(pb.getBreakpointForSize(100), "xxx", "Defining default breakpoints makes the breakpoints automatically get added.");
+    });
+}());
